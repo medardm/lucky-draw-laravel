501ce7ede5e336ed80e2d88ec14ad0937ee23a32
Medard Mandane <medardm13@gmail.com>
2020-03-24 22:45:32 +0800

    Generate tickets

--- app/Models/DrawTicket.php
+++ app/Models/DrawTicket.php
@@ -7,10 +7,45 @@ use App\User;
 
 class DrawTicket extends Model
 {
+    const START = 0000;
+    const END = 9999;
+
     protected $fillable = [
         'ticket_number',
         'user_id'
     ];
+
+    protected $attributes = [
+        'ticket_number' => ''
+    ];
+
+    public function setTicketNumberAttribute($value)
+    {
+        if (empty($value)) {
+            $ticketNumber = self::generateTicket();
+        } else {
+            $ticketNumber = self::ticketExists($value)? self::generateTicket() : $value;
+        }
+
+        $this->attributes['ticket_number'] = $ticketNumber;
+    }
+
+    public static function generateTicket()
+    {
+        do {
+            $ticketNumber = random_int(self::START, self::END);
+        } while (self::ticketExists($ticketNumber));
+
+        return $ticketNumber;
+    }
+
+    public static function ticketExists($ticketNumber)
+    {
+        if (self::count() == (self::END - self::START)) {
+            throw new Exception('Tickets maxed out');
+        }
+        return self::where('ticket_number', $ticketNumber)->exists();
+    }
 
     public function user()
     {
--- tests/Unit/DrawTicketUnitTest.php
+++ tests/Unit/DrawTicketUnitTest.php
@@ -20,4 +20,25 @@ class DrawTicketUnitTest extends TestCase
         $ticket = factory(DrawTicket::class)->create();
         $this->assertInstanceOf(User::class, $ticket->user);
     }
+
+    public function testGenerateTicket()
+    {
+        $randomTicket = factory(DrawTicket::class)->create(['ticket_number' => '']);
+        $this->assertIsInt($randomTicket->ticket_number);
+
+        $ticketNumber = 2124;
+        $customTicket = factory(DrawTicket::class)->create(['ticket_number' => $ticketNumber]);
+        $this->assertEquals($ticketNumber, $customTicket->ticket_number);
+    }
+
+    public function testTicketIsUnique()
+    {
+        $ticketNumber = 2124;
+        $customTicket = factory(DrawTicket::class)->create(['ticket_number' => $ticketNumber]);
+        $this->assertEquals($ticketNumber, $customTicket->ticket_number);
+
+        // generate ticket if entered ticket is duplicate (only for this test)
+        $customTicket2 = factory(DrawTicket::class)->create(['ticket_number' => $ticketNumber]);
+        $this->assertNotEquals($ticketNumber, $customTicket2->ticket_number);
+    }
 }
